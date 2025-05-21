add5cb0645ce14142e8f6b41494fc3fe81c557e9
JeiYoungByun <byeonjeiyoung@gmail.com>
2025-05-21 14:29:32 +0900

    setter injection

--- 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -12,8 +12,12 @@ public class DemoController {
     private Coach myCoach;
 
     // define a constructor for dependency injection
+//    @Autowired
+//    public DemoController(Coach theCoach) {
+//        myCoach = theCoach;
+//    }
     @Autowired
-    public DemoController(Coach theCoach) {
+    public void setCoach(Coach theCoach) {
         myCoach = theCoach;
     }
 
